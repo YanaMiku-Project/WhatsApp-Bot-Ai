dd1b55d224529c2d4bbd746dc487600f5aadce47
YanaMiku-BOTz <123550565+YanaMiku-BOTz@users.noreply.github.com>
2024-01-31 14:25:39 +0700

    Fix Bug Chat Message

--- plugins/ai.js
+++ plugins/ai.js
@@ -1,24 +1,21 @@
 const fetch = require('node-fetch');
 
 exports.run = {
-   async: async (m, { yanamiku, setting, Func, text }) => {
+   async: async (m, { yanamiku, setting, Func }) => {
       try {
-         if (setting.ai === true && m.mtype === 'conversation') {
-         /*const args = m.text.trim().split(/\s+/);
-         const text = args.slice(1).join(' ');*/
-         yanamiku.sendReact(m.chat, '❤️', m.key)
+         if (setting.ai && m.mtype === 'conversation') {
+            yanamiku.sendReact(m.chat, '❤️', m.key);
+            const url = `https://hercai.onrender.com/v3/hercai?question=${encodeURIComponent(m.text)}`;
+            const response = await fetch(url);
+            const json = await response.json();
 
-         const url = `https://hercai.onrender.com/v3/hercai?question=${encodeURIComponent(text)}`;
-         const response = await fetch(url);
-         const json = await response.json();
-
-         if (json.reply) {
-            yanamiku.reply(m.chat, json.reply, m);
-            yanamiku.sendReact(m.chat, '✅', m.key);
-         } else {
-            yanamiku.reply(m.chat, '❌ An error occurred while processing your request. Please try again later.', m);
+            if (json.reply) {
+               yanamiku.reply(m.chat, json.reply, m);
+               yanamiku.sendReact(m.chat, '✅', m.key);
+            } else {
+               yanamiku.reply(m.chat, '❌ An error occurred while processing your request. Please try again later.', m);
+            }
          }
-      }
       } catch (e) {
          console.error(e);
          yanamiku.reply(m.chat, Func.texted('bold', '❌ An error occurred while processing your request. Please try again later.'), m);
@@ -26,4 +23,4 @@ exports.run = {
    },
    error: false,
    location: __filename,
-}
+};
